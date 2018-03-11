15919a4a4f6209fec1306b5e784f7a5ee4a1f2a8
Park <wanbaep2@ug-mac39.ics.illinois.edu>
2018-03-11 18:48:08 -0500

    problem 3 plot add

--- Homework/HW6/plot.R
+++ Homework/HW6/plot.R
@@ -1,19 +1,29 @@
 library(plotrix)
 setwd("/Users/wanbaep2/Desktop/FIN513/Homework/HW6")
+
+# PROBLEM 2
 problem2 = read.csv("problem2.csv")
 
 plot(problem2$OPTION_PRICE, problem2$VOL, xlab = "Stock Price",
      ylab = "Volatility", type ='l',
      main = "Stock Volatility under Merton Model")
 
-from = 5
-to = 100
-gap.plot(problem2$OPTION_PRICE, problem2$CEVVOL, 
+plot(problem2$OPTION_PRICE, problem2$CEVVOL, 
          xlab = "Stock Price",
      ylab = "Volatility", type = 'l',
-     gap = c(from, to),
      main = "Stock Volatility under CEV Model")
-axis.break(2, from, breakcol="snow", style="gap")
-axis.break(2, from*(1+0.02), breakcol="black", style="slash")
-axis.break(4, from*(1+0.02), breakcol="black", style="slash")
-axis(2, at=from)
+
+# PROBLEM 3
+problem3 = read.csv("problem3.csv")
+
+plot(problem3$VOL, problem3$MULT.100,
+     xlab = "Volatility of Firm Value",
+     ylab = "Volatility Multiplier",
+     main = "Volatilty Multiplier, Face Value = 100",
+     type = 'l')
+
+plot(problem3$VOL, problem3$MULT.50,
+     xlab = "Volatility of Firm Value",
+     ylab = "Volatility Multiplier",
+     main = "Volatilty Multiplier, Face Value = 50",
+     type = 'l')
