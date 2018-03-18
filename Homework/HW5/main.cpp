4165aedc123bd49d47b80c6ff44e5fc1f0113e20
wbpark91 <wbpark91@gmail.com>
2018-03-18 17:09:52 -0500

    add description

--- Homework/HW5/main.cpp
+++ Homework/HW5/main.cpp
@@ -10,10 +10,17 @@ int main(void) {
     MarketVariable mktVar(100, 0.0, 0.0, 0.20);
     Straddle strad(100, 1.0);
 
+    int numSim = 10000;
+    int numStep = 500;
+
+    std::cout << "=======Hedge Simulation=======" << std::endl;
+    std::cout << "Number of Simulation: " << numSim << std::endl;
+    std::cout << "Number of Steps: " << numStep << std::endl;
+
     for (int i = 0; i < vol.size(); ++i) {
         mktVar.setVol(vol[i]);
         strad.setMarketVariable(mktVar);
-        std::vector<double> result = strad.hedgeSimulation(10000, 500, 0.00);
+        std::vector<double> result = strad.hedgeSimulation(numSim, numStep, 0.00);
         std::cout << "=======" << "Vol: " << vol[i] << "=======" << std::endl;
         std::cout << "Analytic Payoff: " << strad.expectedPayoff(0.07) << std::endl;
         std::cout << "Expected Payoff: " << result[0] << std::endl;
