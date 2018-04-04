9d4975cc09718616cd8a562046b5d1f8150b7891
Park <wanbaep2@ug-mac30.ics.illinois.edu>
2018-04-03 20:23:04 -0500

    fix initialization of struct

--- Homework/HW7/FIN513_HW7.cpp
+++ Homework/HW7/FIN513_HW7.cpp
@@ -7,12 +7,12 @@
 
 int main(void) {
     OUProcess process(0.03576228, 0.10, 0.04614356, 0.0065, 0.0);
-    SnowballSpread spread(0.0, 0.02, 0.06, 2.0, 0.0050);
+    SnowballSpread spread = {0.0, 0.02, 0.06, 2.0, 0.0050};
     SnowballSwap swap(89.0, 0.03, spread, 15.0, 2.0, Quarter);
     swap.setProcess(process);
 
     std::vector<double> result = swap.mcPrice(10000, 365 * 22);
-    std::cout << "Average: " << average(result) << std::endl;
+    std::cout << "Average: " << mean(result) << std::endl;
     std::cout << "St.dev: " << stdev(result) / sqrt(result.size()) << std::endl;
 
     return 0;
