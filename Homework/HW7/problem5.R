4d15454e7c7a82de374d35e48a290d2b608601fc
Park <wanbaep2@ug-mac42.ics.illinois.edu>
2018-04-06 15:54:47 -0500

    R file

--- Homework/HW7/problem5.R
+++ Homework/HW7/problem5.R
@@ -1,4 +1,9 @@
 setwd("/Users/wanbaep2/Desktop/FIN513/Homework/HW7")
 result = read.csv("problem5.csv")
-hist(result$VALUE, xlab = "Value", main = "Histogram")
+rate = read.csv("rate.csv")
+hist(result$VALUE, xlab = "Value", main = "")
+hist(rate$RATE, xlab = "Rate", main = "Histogram")
 summary(result$VALUE)
+
+print("Number of path which has value below zero: ")
+print(sum(result$VALUE < 0))
