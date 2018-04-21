01a7418dd394a2463e8e26c299ab6063633adaf3
wbpark91 <wbpark91@gmail.com>
2018-04-21 00:40:15 -0500

    plot

--- Homework/HW8/prob2_plot.R
+++ Homework/HW8/prob2_plot.R
@@ -1,10 +1,10 @@
 setwd("/Users/park-wanbae/Desktop/MFE/2018-1/FIN513/Homework/HW8")
 dist = read.csv("prob2_distribution.csv")
 
-plot(dist$X...u, dist$D, type = 'l', xlab = "u", ylab = "CDF",
-     ylim = c(0, 0.05))
+plot(dist$u, dist$D, type = 'l', xlab = "Fraction", ylab = "CDF",
+     ylim = c(0.8, 1))
 par(new = "T")
-plot(dist$X...u, dist$L, type = 'l', xlab = "u", ylab = "CDF",
-     ylim = c(0, 0.05), lty = 2)
+plot(dist$u, dist$L, type = 'l', xlab = "Fraction", ylab = "CDF",
+     lty = 2, ylim = c(0.8, 1))
 legend(0.0, 0.05, legend=c("L", "D"),
        lty=1:2, cex=0.8)
